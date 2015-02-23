45e268662711a6de90a27dc140f164c9d9cc3178
adamkaplan <adamkaplan@yahoo-inc.com>
2015-02-23 16:08:29 -0500

    Revert "Fixed typos from very timely peer reviews"
    This reverts commit fd96d5444ee23c107b3c6a72f062583d01cbb3f1.

--- Demo/YMPromptKitDemo/AppDelegate.m
+++ Demo/YMPromptKitDemo/AppDelegate.m
@@ -119,12 +119,12 @@
 - (BOOL)promptManager:(YMPromptManager *)manager shouldRequestAccessType:(YMPromptAccessType)accessType
                 modes:(YMPromptAccessMode)modes {
     
-    NSArray *promptptLog = [manager.log promptHistory:accessType];
+    NSArray *promptLog = [manager.log promptHistory:accessType];
     
-    if (promptptLog.count > 3) {
+    if (promptLog.count > 3) {
         return NO;                              // enforce the request limit
-    } else if (promptptLog.count) {
-        NSDictionary *dict = promptptLog[0];     // data for the most recent history entry
+    } else if (promptLog.count) {
+        NSDictionary *dict = promptLog[0];     // data for the most recent history entry
         NSDate *lastPromptDate = dict[kYMPromptLogDateKey];
         NSTimeInterval interval = -3600 * 48;   // 2 days, in seconds
         
